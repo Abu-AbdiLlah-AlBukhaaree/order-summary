a9b69adab16186dbdcbf4e0ab3bfbcc89f8049b9
unknown <faisolbamigbade@gmail.com>
2023-08-12 23:03:28 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        paleBlue: 'hsl(225, 100%, 94%)',
+        brightBlue: 'hsl(245, 75%, 52%)',
+        veryLightBlue: 'hsl(225, 100%, 98%)',
+        desaturatedBlue: 'hsl(224, 23%, 55%)',
+        darkBlue: 'hsl(223, 47%, 23%)',
+      },
+    },
+  },
+  plugins: [],
+};
